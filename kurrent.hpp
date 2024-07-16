537de1d75eaa6f316bffa403c9eb3933aa38663a
Kunal Shandilya <155867142+5handilya@users.noreply.github.com>
2024-07-16 20:07:20 +0530

    init

---
+++ kurrent.hpp
@@ -0,0 +1,7 @@
+/**
+ * 1. initialize - random
+ * 2. feedforward matops 
+ * 3. optimized sigmoid
+ * 4. cost function 
+ * 5. you can see how SIMD can be used for a lot of this so put it everywhere
+ */
