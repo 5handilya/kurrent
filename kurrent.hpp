afea67d66f501aa109e068217e16c76d9ba56386
Kunal Shandilya <kunalshandilyawork@gmail.com>
2024-07-17 04:21:26 +0530

    Update kurrent.hpp

--- kurrent.hpp
+++ kurrent.hpp
@@ -5,3 +5,25 @@
  * 4. cost function 
  * 5. you can see how SIMD can be used for a lot of this so put it everywhere
  */
+
+#include <stdio.h>
+#include <iostream>
+#include <immintrin.h>
+#include <chrono>
+#include <math.h>
+#include "..\voltage\voltage.hpp"
+using namespace std;
+using namespace std::chrono;
+const float E = 2.71828182845904523536;
+
+float sigmoid(const float& fl){
+       //naive - vectorize this
+       return (1/(1+pow(E, -1*fl)));
+};
+
+void initialize_weights(){
+    //what data type?
+};
+void feedforward();
+
+void backprop();
